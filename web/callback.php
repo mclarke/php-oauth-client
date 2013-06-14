9b3a915f79f169f69f8389a96a6f188896dfe75b
François Kooman <fkooman@tuxed.net>
2013-06-14 17:56:45 +0200

    add back error page based on Twig template

--- web/callback.php
+++ web/callback.php
@@ -15,7 +15,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use fkooman\Config\Config;
 use fkooman\OAuth\Client\Callback;
@@ -23,20 +23,28 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $app = new Silex\Application();
-$app['debug'] = true;
-$app->get('/', function(Request $request) use ($app) {
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+$app->get('/', function(Request $request) use ($app) {
     $configFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "config.yaml";
     $config = Config::fromYamlFile($configFile);
     $service = new Callback($config);
-
     $returnUri = $service->handleCallback($request);
-
     return $app->redirect($returnUri);
 });
 
-//$app->error(function(\fkooman\OAuth\Client\CallbackException $e, $code) use ($app) {
-//    return new Response($e->getMessage());
-//});
+$app->error(function(\fkooman\OAuth\Client\CallbackException $e, $code) use ($app) {
+    return $app['twig']->render('error.twig', array(
+        'message' => $e->getMessage(),
+    ));
+    
+    // FIXME: also log this!
+    // FIXME: also handle other types of errors
+    // FIXME: also handle errorexceptions
+    return new Response($e->getMessage() . $code, 400);
+});
 
 $app->run();
