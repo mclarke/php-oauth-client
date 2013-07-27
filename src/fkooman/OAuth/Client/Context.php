27a148e72ab93fafb2a734e51a15e999480e4b87
François Kooman <fkooman@tuxed.net>
2013-07-27 16:23:28 +0200

    allow not specifying scope, attempt at fixing issue #10

--- src/fkooman/OAuth/Client/Context.php
+++ src/fkooman/OAuth/Client/Context.php
@@ -7,7 +7,7 @@ class Context
     private $userId;
     private $scope;
 
-    public function __construct($userId, array $scope)
+    public function __construct($userId, array $scope = null)
     {
         $this->setUserId($userId);
         $this->setScope($scope);
@@ -26,22 +26,26 @@ class Context
         return $this->userId;
     }
 
-    public function setScope(array $scope)
+    public function setScope(array $scope = null)
     {
-        if (0 === count($scope)) {
-            throw new ContextException("need to provide at least one scope value");
-        }
-        foreach ($scope as $s) {
-            if (!is_string($s) || 0 >= strlen($s)) {
-                throw new ContextException("scope values need to be non-empty strings");
+        if (null === $scope) {
+            $this->scope = null;
+        } else {
+            if (0 === count($scope)) {
+                throw new ContextException("need to provide at least one scope value");
             }
+            foreach ($scope as $s) {
+                if (!is_string($s) || 0 >= strlen($s)) {
+                    throw new ContextException("scope values need to be non-empty strings");
+                }
+            }
+            // FIXME: we need to validate the scope values
+            $this->scope = array_values(array_unique($scope, SORT_STRING));
         }
-        // FIXME: we need to validate the scope values
-        $this->scope = array_values(array_unique($scope, SORT_STRING));
     }
 
     public function getScope()
     {
-        return implode(" ", $this->scope);
+        return null !== $this->scope ? implode(" ", $this->scope) : null;
     }
 }
